8aa6351336174cd88f0c868ee90d15eb3ecfb2c6
Danielhayward1223 <138681558+Danielhayward1223@users.noreply.github.com>
2024-07-30 21:17:30 -0230

    Problem finished

--- Queues/Main.java
+++ Queues/Main.java
@@ -3,6 +3,27 @@ package Queues;
 public class Main {
     public static void main(String[] args) {
         QueueArray queueArray = new QueueArray(5);
+        Shelter shelter = new Shelter(5);
+
+        queueArray.enQueue(1);
+        queueArray.enQueue(2);
+        queueArray.enQueue(3);
+        queueArray.peek();
+        
+        shelter.enQueue(new Dog(1));
+        shelter.enQueue(new Cat(2));
+        shelter.enQueue(new Dog(3));
+        shelter.enQueue(new Cat(4));
+        shelter.viewAllAnimals();
+        System.out.println("----------------------");
+        shelter.deQueueAny();
+        shelter.viewAllAnimals();
+        System.out.println("----------------------");
+        shelter.deQueueDog();
+        shelter.viewAllAnimals();
+        System.out.println("----------------------");
+        shelter.deQueueCat();
+        shelter.viewAllAnimals();
     }
 
 
